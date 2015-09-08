a82b31ed58ad3edc83c7a1b4205cbc7b3d8be45d
Josh Fabean <josh@codekoalas.com>
2015-09-08 14:47:34 -0500

    testing changes

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-var PeerServer = require('peer').PeerServer;
-var server = new PeerServer({port: 9000, path: '/myapp'});
-var connected = [];
-server.on('connection', function (id) {
-  var idx = connected.indexOf(id); // only add id if it's not in the list yet
-  if (idx === -1) {connected.push(id);}
-});
-server.on('disconnect', function (id) {
-  var idx = connected.indexOf(id); // only attempt to remove id if it's in the list
-  if (idx !== -1) {connected.splice(idx, 1);}
-});
-//someexpressapp.get('/connected-people', function (req, res) {
-//  return res.json(connected);
-//});
+// var PeerServer = require('peer').PeerServer;
+// var server = new PeerServer({port: 9000, path: '/myapp'});
+// var connected = [];
+// server.on('connection', function (id) {
+//   var idx = connected.indexOf(id); // only add id if it's not in the list yet
+//   if (idx === -1) {connected.push(id);}
+// });
+// server.on('disconnect', function (id) {
+//   var idx = connected.indexOf(id); // only attempt to remove id if it's in the list
+//   if (idx !== -1) {connected.splice(idx, 1);}
+// });
 
-
+var connected = ['hi'];
 var express = require('express');
 var app = express();
 
-app.get('/connected-people', function(req,res) {
+app.get('*', function(req,res) {
         return res.json(connected);
 });
-app.listen(3000, function() { console.log('listening')});
+app.listen(8080, function() {
+  console.log('listening');
+});
