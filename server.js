db3c14562b42e7a6a234b273d31f2ebbfa1d65f5
Josh Fabean <josh@codekoalas.com>
2015-09-08 14:49:47 -0500

    added back in peerjs

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// var PeerServer = require('peer').PeerServer;
-// var server = new PeerServer({port: 9000, path: '/myapp'});
-// var connected = [];
-// server.on('connection', function (id) {
-//   var idx = connected.indexOf(id); // only add id if it's not in the list yet
-//   if (idx === -1) {connected.push(id);}
-// });
-// server.on('disconnect', function (id) {
-//   var idx = connected.indexOf(id); // only attempt to remove id if it's in the list
-//   if (idx !== -1) {connected.splice(idx, 1);}
-// });
+var PeerServer = require('peer').PeerServer;
+var server = new PeerServer({port: 9000, path: '/myapp'});
+var connected = [];
+server.on('connection', function (id) {
+  var idx = connected.indexOf(id); // only add id if it's not in the list yet
+  if (idx === -1) {connected.push(id);}
+});
+server.on('disconnect', function (id) {
+  var idx = connected.indexOf(id); // only attempt to remove id if it's in the list
+  if (idx !== -1) {connected.splice(idx, 1);}
+});
 
-var connected = ['hi'];
+
 var express = require('express');
 var app = express();
 
